71ce00e95c0bfe64d440320c8b65dbba491fcb1d
Emerson <emersonbnp@gmail.com>
2016-11-27 11:10:09 -0300

    No 1 - init

--- Projeto Final/node1.c
+++ Projeto Final/node1.c
@@ -17,7 +17,46 @@ void printdt1(t_dt *);
 
 void rtinit1()
 {
+    //----- Inicialização das estruturas
+    int i, j;
+    pkt1 = (struct rtpkt *)malloc( sizeof(struct rtpkt) );
+    pkt1->sourceid = 1; //id do nó
 
+    //printf("NODE0: initialization event at t=%f\n", clocktime );
+    
+    //Custos dos vizinhos conhecidos do nó 1
+    dt1.costs[1][0] = 1; 
+    dt1.costs[1][1] = 0; 
+    dt1.costs[1][2] = 1; 
+    dt1.costs[1][3] = 999;
+
+    //Distâncias entre os nós que não são o nó 1
+    for (i=1; i<4; i++)
+    {   
+        for (j=0; j<4; j++)
+        { 
+            dt1.costs[i][j]=999; //Para facilitar, no exercício foi pedido para considerar infinito como sendo 999
+        }
+    } 
+
+    // preenche a estrutura a ser enviada, pela função tolayer2(), para os vizinhos diretamente conectados ao nó 0(1, 2 e 3, respectivamente),
+    // contendo o custo dos caminhos de custo mínimo para todos os outros nós de rede    
+    for(i=0; i<4; i++){
+        pkt1->mincost[i] = dt1.costs[i][i];
+    }    
+    
+    //----- Exibição
+
+    printf("----- Tabela de distancias inicializada: -----\n");
+    printdt1(&dt1); /* exibe o conteúdo da tabela de distâncias após a inicialização */
+
+    pkt1->sourceid = 0; //id do nó
+
+    //Envia para os nós conectados ao nó 1(1 e 2)
+    pkt1->destid = 1;
+    tolayer2(*pkt1);
+    pkt1->destid = 2;
+    tolayer2(*pkt1);
 }
 
 
